944e64671595975c031f6801c268ddb1094b8e11
Max Base <maxbasecode@gmail.com>
2023-02-09 18:51:43 +0000

    update sorting

--- filterer.go
+++ filterer.go
@@ -7,6 +7,70 @@ import (
 	"strings"
 )
 
+func partition(phoneNumbers []string, left int, right int) int {
+	// Get the pivot
+	pivot := phoneNumbers[right]
+
+	// Create a variable to store the index of the smaller element
+	i := left - 1
+
+	// Loop through the array
+	for j := left; j < right; j++ {
+		// If the current element is smaller than the pivot
+		if phoneNumbers[j] < pivot {
+			// Increment the index of the smaller element
+			i++
+
+			// Swap the current element with the element at the index of the smaller element
+			phoneNumbers[i], phoneNumbers[j] = phoneNumbers[j], phoneNumbers[i]
+		}
+	}
+
+	// Swap the element at the index of the smaller element with the pivot
+	phoneNumbers[i+1], phoneNumbers[right] = phoneNumbers[right], phoneNumbers[i+1]
+
+	// Return the index of the smaller element
+	return i + 1
+}
+
+func quickSort(phoneNumbers []string, left int, right int) {
+	if left < right {
+		// Partition the array
+		pivot := partition(phoneNumbers, left, right)
+
+		// Sort the left side
+		quickSort(phoneNumbers, left, pivot-1)
+
+		// Sort the right side
+		quickSort(phoneNumbers, pivot+1, right)
+	}
+}
+
+// Sort by name (UTF-8)
+func sortPhoneNumbers(phoneNumbers map[string]string) map[string]string {
+	// Create a slice to store the phone numbers
+	var phoneNumbersSlice []string
+
+	// Add the phone numbers to the slice
+	for phoneNumber := range phoneNumbers {
+		phoneNumbersSlice = append(phoneNumbersSlice, phoneNumber)
+	}
+
+	// Sort the phone numbers
+	quickSort(phoneNumbersSlice, 0, len(phoneNumbersSlice)-1)
+
+	// Create a map to store the sorted phone numbers
+	sortedPhoneNumbers := make(map[string]string)
+
+	// Add the phone numbers to the map
+	for _, phoneNumber := range phoneNumbersSlice {
+		sortedPhoneNumbers[phoneNumber] = phoneNumbers[phoneNumber]
+	}
+
+	// Return the sorted phone numbers
+	return sortedPhoneNumbers
+}
+
 func main() {
 	// Open the file
 	file, err := os.Open("contacts.txt")
@@ -51,6 +115,9 @@ func main() {
 		fmt.Println("Error reading file:", err)
 		return
 	}
+
+	// Sort the phone numbers by name
+	phoneNumbers = sortPhoneNumbers(phoneNumbers)
 
 	// Create a new file to store the filtered results
 	resultFile, err := os.Create("filtered_contacts.txt")
