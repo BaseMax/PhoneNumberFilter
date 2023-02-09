edaff54b34814d201e3032a6fc68ad315d9c2735
Max Base <maxbasecode@gmail.com>
2023-02-09 18:48:05 +0000

    complete the go program

--- filterer.go
+++ filterer.go
@@ -1,28 +1,81 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
-// Return string or error
-func file_reads(filepath string) 
-
 func main() {
-	// file path
-	path := "data.txt"
-	// open and read the file
-	file, err := os.Open(path)
+	// Open the file
+	file, err := os.Open("contacts.txt")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error opening file:", err)
+		return
 	}
 	defer file.Close()
 
-	// create a new scanner and read the file line by line
-	scanner := bufio.NewScanner(file)
-	i := 0
-	for scanner.Scan() {
-		if i == 0 {
+	// Create a map to store the phone numbers and their corresponding names
+	phoneNumbers := make(map[string]string)
 
-		fmt.Println(scanner.Text())
+	// Read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		columns := strings.Split(line, "\t")
+
+		// Skip empty lines
+		if len(columns) < 3 {
+			continue
+		}
+
+		// Get the name and phone number
+		name := columns[1]
+		phoneNumber := columns[2]
+
+		// Check if the phone number is already in the map
+		if _, exists := phoneNumbers[phoneNumber]; exists {
+			continue
+		}
+
+		// Trim name
+		name = strings.TrimSpace(name)
+
+		// Add the phone number and name to the map
+		phoneNumbers[phoneNumber] = name
+	}
+
+	// Check for errors while reading the file
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Create a new file to store the filtered results
+	resultFile, err := os.Create("filtered_contacts.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer resultFile.Close()
+
+	// Write the header to the result file
+	header := "ID\tName\tPhone Number\n"
+	resultFile.WriteString(header)
+
+	// Write the filtered results to the result file
+	var id int
+	for phoneNumber, name := range phoneNumbers {
+		id++
+		result := fmt.Sprintf("%d\t%s\t%s\n", id, name, phoneNumber)
+		resultFile.WriteString(result)
+	}
+
+	// Print the filtered results in a table format
+	fmt.Println(header)
+	for phoneNumber, name := range phoneNumbers {
+		id++
+		fmt.Printf("%d\t%s\t%s\n", id, name, phoneNumber)
 	}
 }
